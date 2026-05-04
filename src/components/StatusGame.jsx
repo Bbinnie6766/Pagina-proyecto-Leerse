91ecbea38b0d30aa999531bcbd4ccf52c6d4c39c
cardonero34 <produccionmultimediaemanuel@gmail.com>
2026-05-04 11:05:23 -0500

    funcionalidad botones

--- src/components/StatusGame.jsx
+++ src/components/StatusGame.jsx
@@ -1,28 +1,31 @@
 import { useState } from "react";
 
 export const StatusGame = () => {
-  const [gemas, setGemas] = useState({})
-  const [vidas, setVidas] = useState({})
+  const [gemas, setGemas] = useState()
+  const [vidas, setVidas] = useState()
+  const [characters, setCharacters] = useState(0)
 
   return (
     <>
       <div className="gemas">
-        <button>gema 1</button>
-        <button>gema 2</button>
-        <button>gema 3</button>
+        <h1>💎 Gema: {gemas}</h1>
+        <button onClick={() => setGemas(prev => prev + 1)}>gema 1</button>
+        <button onClick={() => setGemas(prev => prev + 1)}>gema 2</button>
+        <button onClick={() => setGemas(prev => prev + 1)}>gema 3</button>
       </div>
 
       <div className="vidas">
-        <button>Vida 1</button>
-        <button>Vida 2</button>
-        <button>Vida 3</button>
+        <h1>💕 vida: {vidas}</h1>
+        <button onClick={() => setVidas(prev => prev + 1)}>Vida 1</button>
+        <button onClick={() => setVidas(prev => prev + 1)}>Vida 2</button>
+        <button onClick={() => setVidas(prev => prev + 1)}>Vida 3</button>
       </div>
       
       <div className="vcharacters">
-        <h1>🚀 character: _____</h1>
-        <button>character 1</button>
-        <button>character 2</button>
-        <button>character 3</button>
+        <h1>🚀 character: {characters}</h1>
+        <button onClick={() => setCharacters(prev => prev + 1)}>R2D2</button>
+        <button onClick={() => setCharacters(prev => prev + 3)}>Doc Emmet Brown</button>
+        <button onClick={() => setCharacters(prev => prev + 2)}>Pikachu</button>
       </div>
     </>
   )
