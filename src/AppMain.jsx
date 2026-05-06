2bccb2049d6f06f6817889b3dae6505506c7435d
cardonero34 <produccionmultimediaemanuel@gmail.com>
2026-05-06 11:06:59 -0500

    creación pagina Emanuel

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StatusGame } from './components/StatusGame'
+import { EmanuelRMPage } from './pages/EmanuelRMPage'
 
 export const AppMain = () => {
   return (
     <>
     <StatusGame />
+    <EmanuelRMPage />
     </>
   )
 }
